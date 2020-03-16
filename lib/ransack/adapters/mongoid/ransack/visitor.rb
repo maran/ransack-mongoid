99810e0fbf599c09cf970ef0549a5e725e26b5ef
Ondrej Huta <ondrej@mineus.net>
2020-03-16 12:41:01 +0100

    Fix visitor, query

--- lib/ransack/adapters/mongoid/ransack/visitor.rb
+++ lib/ransack/adapters/mongoid/ransack/visitor.rb
@@ -5,34 +5,17 @@ module Ransack
       nodes.inject(&:and)
     end
 
+    def visit_and(object)
+      nodes = object.values.map { |o| accept(o) }.compact
+      nodes.inject(&:or)
+    end
+
     def quoted?(object)
-      case object
-      when Arel::Nodes::SqlLiteral, Bignum, Fixnum
-        false
-      else
-        true
-      end
+      raise "not implemented"
     end
 
     def visit_Ransack_Nodes_Sort(object)
-      return unless object.valid?
-      if object.attr.is_a?(Arel::Attributes::Attribute)
-        object.attr.send(object.dir)
-      else
-        ordered(object)
-      end
+      object.attr.send(object.dir) if object.valid?
     end
-
-    private
-
-      def ordered(object)
-        case object.dir
-        when 'asc'.freeze
-          Arel::Nodes::Ascending.new(object.attr)
-        when 'desc'.freeze
-          Arel::Nodes::Descending.new(object.attr)
-        end
-      end
-
   end
 end
